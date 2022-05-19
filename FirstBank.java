f896234470a3f55b2d2424e01721fbb12c50e55f
joaquinmanresa <87816533+joaquinmanresa@users.noreply.github.com>
2022-05-19 10:03:52 -0300

    Create FirstBank.java

---
+++ FirstBank.java
@@ -0,0 +1,13 @@
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Account tim = new Account ("Usuario", "A00001");
+		
+		Account joaquin = new Account ("Joaquin", "B00002");
+		joaquin.showMenu();
+	}
+
+}
